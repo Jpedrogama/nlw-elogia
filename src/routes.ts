573acefe23e455a60e87e749087ba839aead72ff
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2021-06-25 02:16:08 -0300

    criando as tags

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,16 @@
 import { request, response, Router } from "express";
-
+import { ensureAdmin } from "./middlewares/isAdmin";
 // Importação de Controllers
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagService";
 
 const router = Router();
 
 //Instanciando os controllers
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
+router.post("/tags", ensureAdmin ,createTagController.handle);
 
 export {router}
